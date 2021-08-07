9e243bb76246565d1cfa71b1ad9fb61a3c732731
Rahul Jacob <e.rahuljacob@gmail.com>
2021-08-08 03:46:04 +0530

    Pushing code to trigger jenkins build

--- src/main/java/com/xyz/raul/greeting/GreetingDemoApplication.java
+++ src/main/java/com/xyz/raul/greeting/GreetingDemoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GreetingDemoApplication {
-
+/*
+ * Program Execution begins here...
+ */
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingDemoApplication.class, args);
 	}
