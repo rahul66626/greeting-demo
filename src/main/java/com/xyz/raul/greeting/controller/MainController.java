8c9013944a68b8c2c18e76dfd198f52edab469f9
Rahul Jacob <e.rahuljacob@gmail.com>
2021-08-08 00:33:07 +0530

    Pushing Changes to trigger Jenkins build

---
+++ src/main/java/com/xyz/raul/greeting/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.xyz.raul.greeting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	@GetMapping("/")
+    public String greetMe(){
+        return "Hello World From Docker Container.......";
+    }
+}
